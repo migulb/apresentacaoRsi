c87d424504fddb5e603de4032c76f94e809e544a
Miguel B <migulb44@gmail.com>
2020-01-26 10:48:44 -0300

    Pequenas alterações

--- src/main/java/br/com/rsinet/HUB_TDD/testenegativo/PageObject/PaginaInicialTesteNegativo.java
+++ src/main/java/br/com/rsinet/HUB_TDD/testenegativo/PageObject/PaginaInicialTesteNegativo.java
@@ -1,6 +1,7 @@
 package br.com.rsinet.HUB_TDD.testenegativo.PageObject;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -9,7 +10,8 @@ public class PaginaInicialTesteNegativo {
 	private static WebElement element;
 
 	public static WebElement btn_notebook(WebDriver driver) {
-
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("scrollBy(0,400)", "");
 		element = driver.findElement(By.id("details_10"));
 
 		return element;
